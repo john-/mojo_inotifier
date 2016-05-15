e48d9bde95134af7fa1acf8239e81dfbdbdd726b
johna <john-@users.noreply.github.com>
2016-05-15 10:00:59 -0500

    basic version

--- lib/Inotifier.pm
+++ lib/Inotifier.pm
@@ -2,7 +2,6 @@ package Inotifier;
 use Mojo::Base 'Mojolicious';
 use Mojo::Log;
 
-use Inotifier::Model::MPD;
 
 # This method will run once at server start
 sub startup {
@@ -10,13 +9,11 @@ sub startup {
 
     $self->secrets(['A secret for you']);
 
-    $self->helper(mpd => sub { state $mpd = Inotifier::Model::MPD->new });
-
     # Router
     my $r = $self->routes;
 
     $r->any('/')->to(controller => 'actions', action => 'base');
-    $r->any('/execute')->to(controller => 'actions', action => 'execute');
+    $r->websocket('/wsinit')->to(controller => 'actions', action => 'wsinit');
 }
 
 1;
--- lib/Inotifier/Controller/Actions.pm
+++ lib/Inotifier/Controller/Actions.pm
@@ -9,40 +9,12 @@ sub base {
     $self->render(template => 'base');
 }
 
-sub execute {
-    my $self = shift;
+sub wsinit {
+    my $self => shift;
+    
+    $self->app->log->debug( 'websocket opened' );
+    $self->send( {json => {fu => 'bar'}} );
 
-    my $cmd = lc($self->param('cmd'));
-
-    my $output;
-    if ($cmd eq 'random') {
-        my $actual_count = $self->mpd->random($self->param('count'));
-        $self->mpd->play;     
-        if ($actual_count) {
-            $output = sprintf('Added %s random songs', $actual_count);
-        }
-        $self->stash(message => $output);
-    } elsif ($cmd eq 'play') {
-        $self->mpd->play;     
-    } elsif ($cmd eq 'pause') {
-        $self->mpd->pause;     
-    } elsif ($cmd eq 'skip') {
-        $self->mpd->next;     
-    } elsif ($cmd eq 'vol +') {
-        my $actual_vol = $self->mpd->volume('up');
-        if ($actual_vol) {
-            $output = sprintf('Volume is now %s', $actual_vol);
-        }
-        $self->stash(message => $output);
-    } elsif ($cmd eq 'vol -') {
-        my $actual_vol = $self->mpd->volume('down');
-        if ($actual_vol) {
-            $output = sprintf('Volume is now %s', $actual_vol);
-        }
-        $self->stash(message => $output);
-    }
-
-    $self->render(template => 'base');
 }
 
 1;
